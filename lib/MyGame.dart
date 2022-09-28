806124c73b32c39f04d3bdc84a5eed3f279a9d05
piplupOwo <82926705+piplupOwo@users.noreply.github.com>
2022-09-28 16:24:16 +0800

    Added in music cache, cycle through list of music, music text

--- lib/MyGame.dart
+++ lib/MyGame.dart
@@ -2,12 +2,20 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:ui' hide TextStyle;
+
 
 class MyGame extends FlameGame with SingleGameInstance, HasTappables, HasCollisionDetection{
   MyPlayer player = MyPlayer();
   double gravity = 4;
+  late TextComponent musicText;
+
+  final style = TextStyle(color: BasicPalette.white.color);
+  final regular = TextPaint(style: TextStyle(color: BasicPalette.red.color, fontSize: 20));
+
 
   @override
   Color backgroundColor() => const Color(0x5900D9FF);
@@ -15,7 +23,17 @@ class MyGame extends FlameGame with SingleGameInstance, HasTappables, HasCollisi
   @override
   Future<void> onLoad() async {
 
+    TextComponent musicText = TextComponent(text: 'Tap the music button (on top right)', textRenderer: regular);
+    musicText.x = size[0] / 2;
+    musicText.y = 64.0;
+    musicText.anchor = Anchor.center;
 
+    add(musicText);
+
+    await FlameAudio.audioCache.load('diamondpokecenter.wav');
+    await FlameAudio.audioCache.load('diamondroute101.wav');
+    await FlameAudio.audioCache.load('diamondstart.wav');
+    await FlameAudio.audioCache.load('pip.wav');
 
 
     // screen coordinates
@@ -25,6 +43,7 @@ class MyGame extends FlameGame with SingleGameInstance, HasTappables, HasCollisi
 
     musicBtn musicbtn = musicBtn();
     musicbtn.position = Vector2(size[0] - musicbtn.width, 0);
+    musicBtn.musicText = musicText;
     add(musicbtn);
 
     // MyComponent myComponent = MyComponent();
@@ -73,8 +92,11 @@ class MyPlayer extends SpriteComponent with Tappable{
 }
 
 class musicBtn extends SpriteComponent with Tappable{
+  int counter = 0;
+  static late TextComponent musicText;
 
   musicBtn() : super(size: Vector2.all(128));
+
 
   @override
   Future<void> onLoad() async {
@@ -84,12 +106,38 @@ class musicBtn extends SpriteComponent with Tappable{
   @override
   bool onTapDown(TapDownInfo info) {
     print('Tapped!');
-    FlameAudio.bgm.play('diamondpokecenter.wav');
+
+    switch (counter) {
+      case 0:
+        FlameAudio.bgm.play('diamondpokecenter.wav');
+        musicText.text = 'Current Music: diamondpokecenter.wav\n\nNext Music: diamondroute101.wav\nTap the music button to change to next music';
+        break;
+      case 1:
+        FlameAudio.bgm.play('diamondroute101.wav');
+        musicText.text = 'Current Music: diamondroute101.wav\n\nNext Music: diamondstart.wav\nTap the music button to change to next music';
+        break;
+      case 2:
+        FlameAudio.bgm.play('diamondstart.wav');
+        musicText.text = 'Current Music: diamondstart.wav\n\nNext Music: Silence\nTap the music button to change to next music';
+        break;
+      case 3:
+        FlameAudio.bgm.stop();
+        musicText.text = 'Current Music: Silence\n\nNext Music: diamondpokecenter.wav\nTap the music button to change to next music';
+        counter = -1;
+        break;
+    }
+
+    counter++;
     return true;
   }
 
 
 }
+
+//insert text at the top
+
+
+
 
 
 class MyComponent extends PositionComponent with TapCallbacks {
